dde1885f6d77b548391bd5cc106252b659aa7b50
Otavigs <125999228+Otavig@users.noreply.github.com>
2023-12-03 11:23:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,74 +1,13 @@
-// --------------------
-// DECLARAR AS VARS 
-// --------------------
-
 // URL
-const URL_API = "http://10.111.9.16:3000"
+const URL_API = "http://10.111.9.16:3000";
 
-// NAVBAR
-const btn_Cadastrar = document.getElementById("btn_Cadastrar");
-const btn_Atualizar = document.getElementById("btn_Atualizar");
-const btn_Busca = document.getElementById("btn_Busca");
-const btn_Exibir = document.getElementById("btn_Exibir");
+// Variáveis para a Navbar
+const btnCadastrar = document.getElementById("btn_Cadastrar");
+const btnAtualizar = document.getElementById("btn_Atualizar");
+const btnBusca = document.getElementById("btn_Busca");
+const btnExibir = document.getElementById("btn_Exibir");
 
-// CADASTRO
-const cadastro_nome_midia = document.getElementById("cadastro_nome_midia");
-const cadastro_tipos = document.getElementById("cadastro_tipo"); // Corrigido: troquei para o ID correto
-const cadastro_data_inicio = document.getElementById("cadastro_data_inicio");
-const cadastro_data_fim = document.getElementById("cadastro_data_fim");
-const cadastro_status = document.getElementById("cadastro_status");
-const cadastro_tempo = document.getElementById("cadastro_tempo");
-const cadastro_url = document.getElementById("cadastro_url");
-
-const cadastro_btn_cadastro = document.getElementById("cadastro_btn_cadastro");
-const cadastro_btn_limpar = document.getElementById("cadastro_btn_limpar");
-const cadastro_btn_voltar = document.getElementById("cadastro_btn_voltar");
-
-// ATUALIZAR
-const atualizar_id_editar = document.getElementById("atualizar_id_editar");
-const atualizar_nome_midia = document.getElementById("atualizar_nome_midia"); // Corrigido: estava com erro de digitação
-const atualizar_tipos = document.getElementById("atualizar_tipos");
-const atualizar_data_inicio = document.getElementById("atualizar_data_inicio");
-const atualizar_data_fim = document.getElementById("atualizar_data_fim");
-const atualizar_status = document.getElementById("atualizar_status");
-const atualizar_tempo = document.getElementById("atualizar_tempo");
-const atualizar_url = document.getElementById("atualizar_url");
-
-const atualizar_btn_dados = document.getElementById("atualizar_btn_dados");
-const atualizar_btn_limpar = document.getElementById("atualizar_btn_limpar");
-const atualizar_btn_voltar = document.getElementById("atualizar_btn_voltar")
-
-
-// BUSCA
-let modoEdicaoAtivo = false;
-const input_busca = document.getElementById("input_busca");
-const busca_opcoes = document.getElementById("busca_opcoes");
-const busca_btn_verificar = document.getElementById("busca_btn_verificar");
-const busca_saida = document.getElementById("busca_saida");
-
-// ---------------------
-// FUNÇÕES DO CÓDIGO
-// ---------------------
-
-
-/**
- * Função para atualizar o ocultar
- */
-
-function toggleOculto() {
-    var navItem = document.querySelector('.oculto');
-
-    if (navItem.classList.contains('oculto')) {
-        navItem.classList.remove('oculto');
-    } else {
-        navItem.classList.add('oculto');
-    }
-}
-
-/**
- * Função para mostrar/ocultar divs
- */
-
+// Função para mostrar/ocultar divs
 function mostrarDiv(id) {
     // Obtém todas as divs
     var divs = document.querySelectorAll('.content');
@@ -83,200 +22,242 @@ function mostrarDiv(id) {
     // Obtém a div desejada
     var div = document.getElementById(id);
 
-    // Verifica o estado atual da div
-    var estaVisivel = div.style.display === 'block';
-
     // Alterna a visibilidade da div
-    div.style.display = estaVisivel ? 'none' : 'block';
+    div.style.display = div.style.display === 'block' ? 'none' : 'block';
 }
 
 
-/**
- * Função para limpar os campos do formulário de cadastro
- */
+// Função para limpar os campos do formulário de cadastro
+function limparFormularioCadastro() {
+    // Limpar os campos conforme necessário
+    cadastro_nome_midia.value = "";
+    cadastro_tipo.value = "";
+    cadastro_data_inicio.value = "";
+    cadastro_data_fim.value = "";
+    cadastro_status.value = "";
+    cadastro_tempo.value = "";
+    cadastro_url.value = "";
+}
 
-
-
-// ---------------------
-// EVENTOS DO CÓDIGO
-// ---------------------
+// Função para limpar os campos do formulário de atualização
+function limparFormularioAtualizacao() {
+    // Limpar os campos conforme necessário
+    atualizar_nome_midia.value = "";
+    atualizar_tipos.value = "";
+    atualizar_data_inicio.value = "";
+    atualizar_data_fim.value = "";
+    atualizar_status.value = "";
+    atualizar_tempo.value = "";
+    atualizar_url.value = "";
+    atualizar_id_editar.value = "";
+}
 
 // Evento cadastrar
-btn_Cadastrar.addEventListener("click", async () => {
-    let nome = document.getElementById("cadastro_nome_midia").value
-    let tipo = document.getElementById("cadastro_tipo").value
-    let data_inicio = document.getElementById("cadastro_data_inicio").value
-    let data_fim = document.getElementById("cadastro_data_fim").value
-    let status = document.getElementById("cadastro_status").value
-    let tempo = document.getElementById("cadastro_tempo").value
-    let url = document.getElementById("cadastro_url").value
+btnCadastrar.addEventListener("click", async () => {
+    try {
+        const nome = cadastro_nome_midia.value;
+        const tipo = cadastro_tipo.value;
+        const data_inicio = cadastro_data_inicio.value;
+        const data_fim = cadastro_data_fim.value;
+        const status = cadastro_status.value;
+        const tempo = cadastro_tempo.value;
+        const url = cadastro_url.value;
 
-    let dados = await fetch(`${URL_API}/api/midia_indoor`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome: nome, tipo: tipo, data_inicio: data_inicio, data_fim: data_fim, status: status, tempo: tempo, ur: url }),
-    });
+        const response = await fetch(`${URL_API}/api/midia_indoor`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                nome: nome,
+                tipo: tipo,
+                data_inicio: data_inicio,
+                data_fim: data_fim,
+                status: status,
+                tempo: tempo,
+                url: url,
+            }),
+        });
 
-    if (dados.ok) {
-        btn_Busca.click()
-        busca_btn_verificar.click()
+        if (resposta && resposta.ok) {
+            mostrarDiv('divResultado'); // Mostrar a div desejada após buscar
+            atualizarTabela(array_resultado);
+        } else {
+            console.error("Erro ao buscar.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição:", error);
     }
+});
 
-})
-
-// Evento buscar {REFAZER COM AS NOVAS OPÇÕES}
-
-
+// Evento buscar
 busca_btn_verificar.addEventListener("click", async () => {
-    let busca = document.getElementById("input_busca").value;
-    let opcao = document.getElementById("busca_opcoes").value;
+    try {
+        const busca = input_busca.value;
+        const opcao = busca_opcoes.value;
+
+        let resposta = null;
+
+        if (opcao === "todos") {
+            resposta = await fetch(`${URL_API}/api/midia_indoor`);
+        } else if (opcao === "id") {
+            resposta = await fetch(`${URL_API}/api/midia_indoor/id/${busca}`);
+        } else if (opcao === "nome") {
+            resposta = await fetch(`${URL_API}/api/midia_indoor/nome/${busca}`);
+        }
+
+        if (resposta && resposta.ok) {
+            const array_resultado = await resposta.json();
+            atualizarTabela(array_resultado);
+        } else {
+            console.error("Erro ao buscar.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição:", error);
+    }
+});
+
+// Função para atualizar a tabela de busca
+function atualizarTabela(data) {
+    const tbody = document.getElementById("busca_saida");
+
     let html = `<table class="table">
                     <thead>
-                    <tr>    
-                        <th scope="col">id</th>
-                        <th scope="col" class='text-start'>Nome</th>
-                        <th scope="col" class='text-start'>Url</th>
-                        <th scope="col">Editar</i></th>
-                        <th scope="col">Excluir</th>
-                    </tr>
+                        <tr>    
+                            <th scope="col">id</th>
+                            <th scope="col" class='text-start'>Nome</th>
+                            <th scope="col" class='text-start'>Url</th>
+                            <th scope="col">Editar</th>
+                            <th scope="col">Excluir</th>
+                        </tr>
                     </thead>
                     <tbody>`;
 
-    document.getElementById("busca_saida").innerHTML = "";
-    document.getElementById("input_busca").value = ""
-
-    let resposta = "";
-    if (opcao == "todos") {
-        resposta = await fetch(`${URL_API}/api/midia_indoor`);
-    } else if (opcao == "id") {
-        resposta = await fetch(`${URL_API}/api/midia_indoor/id/${busca}`);
-    } else if (opcao == "nome") {
-        resposta = await fetch(`${URL_API}/api/midia_indoor/nome/${busca}`);
+    if (Array.isArray(data)) {
+        data.forEach((item) => {
+            html += `<tr>
+                        <td>${item.id}</td>
+                        <td class='text-start'>${item.nome}</td>
+                        <td class='text-start'>${item.url}</td>
+                        <td><i onclick="editar(${item.id})" class="bi bi-pencil"></i></td>
+                        <td><i onclick="excluir(${item.id})" class="bi bi-trash"></i></td>
+                    </tr>`;
+        });
+    } else {
+        html += `<tr>
+                    <td>${data.id}</td>
+                    <td class='text-start'>${data.nome}</td>
+                    <td class='text-start'>${data.url}</td>
+                    <td><i onclick="editar(${data.id})" class="bi bi-pencil"></i></td>
+                    <td><i onclick="excluir(${data.id})" class="bi bi-trash"></i></td>
+                </tr>`;
     }
 
-    if (resposta.ok) {
-        html = html;
-        let array_resultado = await resposta.json();
-        if (opcao == "todos" || opcao == "nome") {
-            for (const dados of array_resultado) {
-                html += `<tr>                
-            <td>${dados.id}</td>
-            <td class='text-start'>${dados.nome}</td>
-            <td class='text-start'>${dados.url}</td>
-            <td><i onclick="editar(${dados.id})" class="bi bi-pencil"></td>
-            <td><i onclick="excluir(${dados.id})" class="bi bi-trash"></i></td>
-            </tr>`;
-            }
-        } else if (opcao == "id") {
-            html += `<tr>                
-            <td>${array_resultado.id}</td>
-            <td class='text-start'>${array_resultado.nome}</td>
-            <td class='text-start'>${array_resultado.url}</td>
-            <td><i class="bi bi-pencil"></td>
-            <td><i class="bi bi-trash"></i></td>
-            </tr>`;
-        }
+    html += `</tbody></table>`;
 
-        html += `</tbody></table>`;
-    }
-    document.getElementById("busca_saida").innerHTML = html;
-
-
-});
-
+    tbody.innerHTML = html;
+}
 
 // Evento para atualizar os dados
+atualizar_btn_dados.addEventListener("click", async () => {
+    try {
+        const id = atualizar_id_editar.value;
+        const nome_atualizado = atualizar_nome_midia.value;
+        const tipo_atualizado = atualizar_tipos.value;
+        const data_inicio_atualizado = atualizar_data_inicio.value;
+        const data_fim_atualizado = atualizar_data_fim.value;
+        const status_atualizado = atualizar_status.value;
+        const tempo_atualizado = atualizar_tempo.value;
+        const url_atualizado = atualizar_url.value;
 
-atualizar_btn_voltar.addEventListener("click", async () => {
-    let nome_atualizado = document.getElementById("atualizar_nome_midia").value
-    let tipo_atualizado = document.getElementById("atualizar_tipos").value
-    let data_inicio_atualizado = document.getElementById("atualizar_data_inicio").value
-    let data_fim_atualizado = document.getElementById("atualizar_data_fim").value
-    let status_atualizado = document.getElementById("atualizar_status").value
-    let tempo_atualizado = document.getElementById("atualizar_tempo").value
-    let url_atualizado = document.getElementById("atualizar_url").value
-    let id = document.getElementById("atualizar_id_editar").value
+        const response = await fetch(`${URL_API}/api/midia_indoor`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                id: id,
+                nome: nome_atualizado,
+                tipo: tipo_atualizado,
+                data_inicio: data_inicio_atualizado,
+                data_fim: data_fim_atualizado,
+                status: status_atualizado,
+                tempo: tempo_atualizado,
+                url: url_atualizado,
+            }),
+        });
 
-
-    let dados = await fetch(`${URL_API}/api/midia_indoor`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: id, nome: nome_atualizado, tipo: tipo_atualizado, data_inicio: data_inicio_atualizado, data_fim: data_fim_atualizado, status: status_atualizado, tempo: tempo_atualizado, url: url_atualizado }),
-    });
-
-    if (dados.ok) {
-        btn_Busca.click()
-        busca_btn_verificar.click()
+        if (response.ok) {
+            mostrarDiv('divBusca'); // Mostrar a div desejada após atualizar
+            busca_btn_verificar.click();
+            limparFormularioAtualizacao();
+        } else {
+            console.error("Erro ao atualizar.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição:", error);
     }
-
-})
-
-/* Função para editar usuário
-    */
-
-async function editar(id) {
-
-    let resposta = await fetch(`${URL_API}/api/midia_indoor/id/${id}`);
-
-    if (resposta.ok) {
-        let dados = await resposta.json();
-        console.clear()
-        console.log(dados)
-        atualizar_btn_dados.click()
-
-        document.getElementById("atualizar_nome_midia").value = dados.nome
-        document.getElementById("atualizar_tipos").value = dados.tipo
-        document.getElementById("atualizar_data_inicio").value = dados.data_inicio
-        document.getElementById("atualizar_data_fim").value = dados.data_fim
-        document.getElementById("atualizar_status").value = dados.status
-        document.getElementById("atualizar_tempo").value = dados.tempo
-        document.getElementById("atualizar_url").value = dados.url
-        document.getElementById("atualizar_id_editar").value = dados.id
-
-    }
-}
-
-
-/* Função para excluir usuário
-*/
-
-
-async function excluir(id) {
-
-    let dados = await fetch(`${URL_API}/api/midia_indoor/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-
-    if (dados.ok) {
-        btn_Busca.click()
-        busca_btn_verificar.click()
-    }
-
-}
-
-
-// Evento para limpar os inputs do cadastrar
-cadastro_btn_limpar.addEventListener("click", () => {
-    // Chama a função para limpar os campos
-    limparFormularioCadastro();
 });
 
-// Evento para limpar os inputs do Atualizar
-atualizar_btn_limpar.addEventListener("click", () => {
-    // Chama a função para limpar os campos
-    limparFormularioAtualizacao();
+// Evento para excluir dados
+excluir_btn_dados.addEventListener("click", async () => {
+    try {
+        const id = atualizar_id_editar.value;
+
+        const response = await fetch(`${URL_API}/api/midia_indoor/${id}`, {
+            method: "DELETE",
+        });
+
+        if (response.ok) {
+            mostrarDiv('divBusca'); // Mostrar a div desejada após excluir
+            busca_btn_verificar.click();
+            limparFormularioAtualizacao();
+        } else {
+            console.error("Erro ao excluir.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição:", error);
+    }
 });
 
 
+// Função para editar dados
+function editar(id) {
+    try {
+        fetch(`${URL_API}/api/midia_indoor/id/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                atualizar_id_editar.value = data.id;
+                atualizar_nome_midia.value = data.nome;
+                atualizar_tipos.value = data.tipo;
+                atualizar_data_inicio.value = data.data_inicio;
+                atualizar_data_fim.value = data.data_fim;
+                atualizar_status.value = data.status;
+                atualizar_tempo.value = data.tempo;
+                atualizar_url.value = data.url;
+            });
+            mostrarDiv('divAtualizar'); // Mostrar a div desejada ao editar
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
+    }
 
-// Copyright @ 2023
-console.log("        /\\_/\\ Copyright © 2023");
-console.log("       ( o.o ) Otávio, Breno, Isa e Kauana.");
-console.log("        > ^ < Todos os direitos reservados.");
+// Função para excluir dados
+function excluir(id) {
+    if (confirm("Deseja realmente excluir?")) {
+        try {
+            fetch(`${URL_API}/api/midia_indoor/${id}`, {
+                method: "DELETE",
+            })
+                .then(response => response.json())
+                .then(data => {
+                    btn_Busca.click();
+                    busca_btn_verificar.click();
+                    limparFormularioAtualizacao();
+                });
+                mostrarDiv('divBusca'); // Mostrar a div desejada após confirmar a exclusão
+            } catch (error) {
+                console.error("Erro na requisição:", error);
+            }
+        }
+    }
