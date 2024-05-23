6b41ce0bc9c94cf14d54800023608f35f8cb4711
Otávio <otaviogarcia.santos4@gmail.com>
2024-05-23 15:19:07 -0300

    atualização_upload_estrutural
    - Mudanças na estrutura
    - Codigo arrumado upload

---
+++ back_end/app.js
@@ -0,0 +1,169 @@
+const fs = require("fs")
+const path = require("path")
+
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql2/promise")
+const bodyparser = require("body-parser")
+
+const app = express()
+app.use(cors())
+const porta = 3000
+app.use(bodyparser.json())
+const multer = require("multer")
+const mw_upload = multer({ dest: "./dir_uploads" })
+
+
+
+// criar uma pool de conexão
+const pool = mysql.createPool({
+    host: `localhost`,
+    user: `root`,
+    password: ``,
+    database: `midia_indoor`,
+    waitForConnections: true,
+    connectionLimit: 3,
+    queueLimit: 0
+
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/front-end/index.html')
+})
+
+app.get('/exibir', (req, res) => {
+    res.sendFile(__dirname + '/front-end/midia.html')
+})
+
+// Rota para obter todas as mídias
+app.get("/api/midia_indoor/", async (req, res) => {
+    try {
+        const conexao = await pool.getConnection();
+        const sql = `SELECT * FROM midia`;
+        const [linha] = await conexao.execute(sql);
+        conexao.release();
+        res.json(linha);
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`);
+        res.status(500).json({ error: "Deu algum erro na busca" });
+    }
+});
+
+app.use("", express.static("front-end/public"))
+
+
+app.post("/upload", mw_upload.single("arquivo"), (req, res) => {
+    try {
+        console.log(req.file);
+
+        if (!req.file) {
+            return res.status(400).json({ error: "Nenhum arquivo foi enviado." });
+        }
+
+        let arq_recebido = req.file.path;
+        let arq_destino = "front-end/public/midias/" + req.file.filename + path.extname(req.file.originalname);
+
+        // Movendo arquivo recebido
+        fs.renameSync(arq_recebido, arq_destino);
+
+        res.status(200).json({
+            message: "Arquivo recebido e movido com sucesso.",
+            filename: req.file.filename + path.extname(req.file.originalname),
+        });
+    } catch (error) {
+        console.error("Erro no upload:", error);
+        res.status(500).json({ error: "Erro interno durante o upload." });
+    }
+});
+
+
+// rota pra SELECT com o ID
+app.get(`/api/midia_indoor/id/:id`, async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(id);
+        const conexao = await pool.getConnection();
+        const sql = `SELECT * FROM midia WHERE id = ${id}`;
+
+        const [linha] = await conexao.execute(sql);
+        console.log(sql);
+        conexao.release();
+        res.json(linha[0]);
+
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`)
+        res.send(500).json({ error: "Deu algum erro na busca" })
+    }
+})
+
+// Rota para SELECT com o nome
+app.get(`/api/midia_indoor/nome/:nome`, async (req, res) => {
+    try {
+        const nome = req.params.nome;
+        const conexao = await pool.getConnection();
+        const sql = `SELECT * FROM midia WHERE nome LIKE "%${nome}%"`;
+        const [linha] = await conexao.execute(sql);
+        console.log(sql);
+        conexao.release();
+        res.json(linha);
+
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`)
+        res.send(500).json({ error: "Deu algum erro na busca" })
+    }
+})
+
+// rota para cadastrar 
+app.post("/api/midia_indoor/", async (req, res) => {
+    try {
+        const { nome, tipo, status, data_inicio, data_fim, url, tempo } = req.body;
+        const conexao = await pool.getConnection();
+        const sql = `INSERT INTO midia (nome, tipo, status, data_inicio, data_fim, url, tempo) VALUES ("${nome}", "${tipo}", "${status}", "${data_inicio}", "${data_fim}", "${url}", ${tempo})`;
+        console.log(sql);
+        const [linha] = await conexao.execute(sql);
+        conexao.release();
+        res.json({ msg: "Registro gravado!" });
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`);
+        res.status(500).json({ error: "Deu algum erro no cadastro" });
+    }
+});
+
+
+// Rota para Editar
+app.put("/api/midia_indoor/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, tipo, status, data_inicio, data_fim, url, tempo } = req.body;
+
+        const conexao = await pool.getConnection();
+        const sql = `UPDATE midia SET nome = "${nome}", tipo = "${tipo}", status = "${status}", data_inicio = "${data_inicio}", data_fim = "${data_fim}", url = "${url}", tempo = "${tempo}" WHERE id = "${id}"`;
+        const [linha] = await conexao.execute(sql);
+        conexao.release();
+
+        res.json({ msg: "Registro Atualizado!" });
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`);
+        res.status(500).json({ error: "Deu algum erro na atualização" });
+    }
+});
+
+// Rota pra deletar
+app.delete("/api/midia_indoor/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const conexao = await pool.getConnection();
+        const sql = `DELETE FROM midia WHERE id = ${id}`;
+        const [linha] = await conexao.execute(sql, [id]);
+        conexao.release();
+        res.json({ msg: "Registro excluído!" });
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`);
+        res.status(500).json({ error: "Deu algum erro na exclusão" });
+    }
+});
+
+
+app.listen(porta, () => {
+    console.log(`o servidor está rodando em http://localhost:${porta}`)
+})
---
+++ back_end/front-end/public/js/carousel.js
@@ -0,0 +1,91 @@
+// URL
+const URL_API = "http://localhost:3000";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    let resposta = await fetch(`${URL_API}/api/midia_indoor`);
+    if (resposta.ok) {
+      let dados = await resposta.json();
+      console.log(dados)
+
+      const carousel = document.getElementById("carousel_midia");
+      let currentIndex = 0; // Índice da mídia atual no array
+
+      function goFullscreen(iframe) {
+        if (iframe.requestFullscreen) {
+          iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) {
+          iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) {
+          iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) {
+          iframe.msRequestFullscreen();
+        }
+      }
+
+      function isYouTubeURL(url) {
+        return url.toLowerCase().includes("youtube.com");
+      }
+
+      function mostrarProximaMidia() {
+        // Remove a mídia atual do carrossel
+        carousel.innerHTML = '';
+
+        const midiaAtual = dados[currentIndex];
+        let url_midia = midiaAtual.url;
+        let tipo_midia = midiaAtual.tipo;
+        let tempo_exibicao = midiaAtual.tempo;
+        let status_midia = midiaAtual.status.toLowerCase();
+        let data_inicio = new Date(midiaAtual.data_inicio);
+        let data_fim = new Date(midiaAtual.data_fim);
+        let data_atual = new Date();
+
+        // Formatação da data para o formato "YYYY-MM-DD"
+        let data_atual_formatada = parseInt(new Date(data_atual).toISOString().slice(0, 10).replace(/-/g, ''));
+        let data_inicio_formatada = parseInt(data_inicio.toISOString().slice(0, 10).replace(/-/g, ''));
+        let data_fim_formatada = parseInt(data_fim.toISOString().slice(0, 10).replace(/-/g, ''));
+
+        console.log(`URL: ${url_midia}, Tipo: ${tipo_midia}, Tempo de Exibição: ${tempo_exibicao} segundos, Status: ${status_midia}, Data Inicial: ${data_inicio_formatada}, Data do fim ${data_fim_formatada} e Data de hoje ${data_atual_formatada}`);
+
+        const div = document.createElement("div");
+        div.className = "carousel-item";
+
+        // Verifica se a mídia está ativa e se a data atual está entre a data de início e de término
+        if (status_midia === 'a' && data_atual_formatada >= data_inicio_formatada && data_atual_formatada <= data_fim_formatada) {
+          if (tipo_midia === 'I') {
+            // Se for uma imagem
+            div.innerHTML = `<img src="midias/${url_midia}" data-tempo="${tempo_exibicao}">`;
+          } else if (tipo_midia === 'V') {
+            // Se for um vídeo local
+            if (isYouTubeURL(url_midia)) {
+              // Se for um vídeo do YouTube, usa a API de incorporação do YouTube para embutir o vídeo
+              div.innerHTML = `<iframe width="100%" height="100%" src="${url_midia}" frameborder="0" allowfullscreen></iframe>`;
+              const iframe = div.querySelector('iframe');
+              goFullscreen(iframe);
+            } else {
+              // Se não for um vídeo do YouTube e for um vídeo local
+              div.innerHTML = `<video src="${url_midia}" data-tempo="${tempo_exibicao}" muted autoplay onended="mostrarProximaMidia()"></video>`;
+            }
+          } else {
+            // Se for outro tipo de mídia, pule para o próximo item
+            mostrarProximaMidia();
+            return;
+          }
+
+          carousel.appendChild(div);
+          currentIndex = (currentIndex + 1) % dados.length;
+          setTimeout(mostrarProximaMidia, tempo_exibicao);
+        } else {
+          currentIndex = (currentIndex + 1) % dados.length;
+          mostrarProximaMidia();
+        }
+      }
+
+      // Inicia o carrossel
+      mostrarProximaMidia();
+
+    }
+  } catch (error) {
+    console.error("Erro ao obter dados da API:", error);
+  }
+});
